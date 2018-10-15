faa5771a8a703ad87b6be98c70523b27cf3e5567
Michael Hewner <hewner@gmail.com>
2018-10-15 12:06:40 -0400

    arcade game project

---
+++ Homework/ArcadeGameProject/src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * The main class for your arcade game.
+ * 
+ * You can design your game any way you like, but make the game start
+ * by running main here.
+ * 
+ * Also don't forget to write javadocs for your classes and functions!
+ * 
+ * @author Buffalo
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Write your cool arcade game here!");
+	}
+
+}
