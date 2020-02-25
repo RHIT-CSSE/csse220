138a3519e69f562130ade25d3b35b6c37f3db465
Aaron Wilkin <4awilkin@gmail.com>
2020-02-25 17:08:49 -0500

    New schedule changes for Spring 202030

--- Administration/gen_dates.rb
+++ Administration/gen_dates.rb
@@ -1,11 +1,11 @@
 require 'date'
 require 'yaml'
 
-start_date = Date.new(2019, 12, 2)
-end_date = Date.new(2020, 2, 21)
-vacation_start = Date.new(2019, 12, 21) #inclusive
-vacation_end = Date.new(2020, 1, 5) # inclusive
-allowed_days_of_week = [1,2,4] # monday is 1
+start_date = Date.new(2020, 3, 9)
+end_date = Date.new(2020, 5, 22)
+vacation_start = Date.new(2020, 4, 10) #inclusive
+vacation_end = Date.new(2020, 4, 19) # inclusive
+allowed_days_of_week = [2,3,5] # monday is 1
 
 day_list = []
 current = start_date;
