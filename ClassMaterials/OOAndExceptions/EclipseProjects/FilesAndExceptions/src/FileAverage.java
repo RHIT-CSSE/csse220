fa5662ef3a9306a80ad96a69fbfa73f79b3a149d
Dr. Holly <hollings@rose-hulman.edu>
2021-10-27 16:54:21 -0400

    updated in-class demo pkg

--- ClassMaterials/OOAndExceptions/EclipseProjects/FilesAndExceptions/src/FileAverage.java
+++ ClassMaterials/OOAndExceptions/EclipseProjects/FilesAndExceptions/src/FileAverage.java
@@ -1,46 +1,71 @@
-
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+
+/*
+ * Note on the use of System.err to output error message
+ * 
+ * System.err is used in the program below, it is the "standard" error output stream in Java.
+ * This stream is already open and ready to accept output data.
+ * 
+ * Typically this stream corresponds to display output or another output destination specified 
+ * by the host environment or user. 
+ * 
+ * By convention, this output stream is used to display error messages or other information 
+ * that should come to the immediate attention of a user even if the principal output stream, 
+ * the value of the variable out, has been redirected to a file or other destination that is 
+ * typically not continuously monitored.
+ */
+
 public class FileAverage {
 
-	public static void main(String[] args) {
-
-		//TODO: Run to see one exception. Trace the code to understand. 
-		// Then change to nums.txt to see the other exception. 
-		String filename = "nums_wrongname.txt"; 
-		try {
-			double average = computeAverageForFile(filename);
-			System.out.println("Average: " + average);
-		} catch(FileNotFoundException e) {
-			System.err.println("File " + filename + " not found.  Exiting.");
-		} catch(IOException e) {
-			System.err.println("Error closing file.");
-		}
-	}
-
-	public static double computeAverageForFile(String filename) throws IOException {
+	private double computeAverageForFile(String filename) throws IOException {
 		double total = 0;
 		int count = 0;
-		FileReader file = new FileReader(filename);
-		Scanner s = new Scanner(file);
+		FileReader f1 = new FileReader(filename);
+		Scanner s1 = new Scanner(f1);
 
-		while(s.hasNext()) {
+		while(s1.hasNext()) {
 			try {
-				total = total + s.nextInt();
+				total = total + s1.nextInt();
 				count++;
 			} catch (InputMismatchException e) {
-				String nonNumber = s.next();
-				System.err.println("Non-number " + nonNumber + " found.  Ignoring.");
-			}
-		}
-		file.close();
-		double average = total/count;
-		return average;
-	}
+				String nonNumber = s1.next();
+				System.err.println("Non-integer " + nonNumber + " found.  Ignoring.");
+			} // end try-catch	
+		} // end while
+		s1.close();
+		f1.close();
+		return total / count;
+	} // computeAverageForFile
+	
+	//-------------------------------------------------------------------------------
+	// Note: FileNotFoundException and IOException are both checked exceptions
+	//-------------------------------------------------------------------------------
+	
+	private void runApp() {
+		//TODO: Run to see one exception. Trace the code to understand. 
+		// Then change to nums.txt to see the other exception. 
+		
+//		String inputFile = "nums_wrongname.txt"; 
+		String inputFile = "nums.txt";
+		try {
+			double average = computeAverageForFile(inputFile);
+			System.out.println("Average: " + average);
+		} catch(FileNotFoundException e) {
+			System.err.println("File " + inputFile + " not found.  Exiting.");
+		} catch(IOException e) {
+			System.err.println("Error closing file.");
+		} // end try-catch	
+	} // runApp
+	
+	//-------------------------------------------------------------------------------
 
+	public static void main(String[] args) {
+		FileAverage app = new FileAverage();
+		app.runApp();
+	} // main
 }
--- ClassMaterials/OOAndExceptions/EclipseProjects/FilesAndExceptions/src/FileBestScore.java
+++ ClassMaterials/OOAndExceptions/EclipseProjects/FilesAndExceptions/src/FileBestScore.java
@@ -1,5 +1,3 @@
-
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -33,45 +31,57 @@ import java.util.Scanner;
  * in main to do this.
  *
  * @author hewner.
- *         Created Apr 21, 2017.
+ *         Created Apr 21, 2017. Updated 2021
  */
-public class FileBestScore {
+public class FileBestScore {	
 
-	public static void main(String[] args) throws IOException {
+	private int computeTotal(String allScoresString) {
+		String[] allScoresArray = allScoresString.split(",");
+		int total = 0;
+		for(String score : allScoresArray) {
+			total += Integer.parseInt(score.trim());
+		} // end for
+		return total;
+	} // computeTotal
 
+	//-------------------------------------------------------------------------------
+
+	private void computeBestScore(String filename) throws FileNotFoundException, IOException {
+		FileReader f1 = new FileReader(filename);
+		Scanner s1 = new Scanner(f1);
+		int bestScore = 0;
+		String bestPlayer = null;
+		
+		while(s1.hasNext()) {
+			String name = s1.next();
+			String allScoresString = s1.nextLine();
+			int total = computeTotal(allScoresString);
+			if(total > bestScore) {
+				bestScore = total;
+				bestPlayer = name;
+			} // end if
+		} // end while
+		s1.close();
+		f1.close();
+		System.out.println("Best player " + bestPlayer + " with total score " + bestScore);
+	} // computeBestScore
+	
+	//-------------------------------------------------------------------------------
+
+	private void runApp() throws IOException {
 		Scanner s = new Scanner(System.in);
 		String filename = null;
 		System.out.println("What file should I load?  (e.g. scores.txt)");
 		filename = s.nextLine();
 		computeBestScore(filename);
-	}
+		s.close();		
+		System.out.println("App terminated");
+	} // runIt
+	
+	//-------------------------------------------------------------------------------
 
-	public static void computeBestScore(String filename) throws FileNotFoundException, IOException {
-		FileReader file = new FileReader(filename);
-		Scanner s = new Scanner(file);
-		int bestScore = 0;
-		String bestPlayer = null;
-		
-		while(s.hasNext()) {
-			String name = s.next();
-			String allScoresString = s.nextLine();
-			int total = computeTotal(allScoresString);
-			if(total > bestScore) {
-				bestScore = total;
-				bestPlayer = name;
-			}
-		}
-		file.close();
-		System.out.println("Best player " + bestPlayer + " with total score " + bestScore);
-	}
-
-	public static int computeTotal(String allScoresString) {
-		String[] allScoresArray = allScoresString.split(",");
-		int total = 0;
-		for(String score : allScoresArray) {
-			total = total + Integer.parseInt(score.trim());
-		}
-		return total;
-	}
-
+	public static void main(String[] args) throws IOException {
+		FileBestScore app = new FileBestScore();
+		app.runApp();
+	} // main
 }
