f12c253cb4651322f1d743a60493ff81b0a2be18
jasonayoder <jasonayoder@gmail.com>
2023-02-26 16:15:35 -0500

    fix help citation

--- Homework/HW2DArrays/src/Homework2DArrays/Homework2DArrays.java
+++ Homework/HW2DArrays/src/Homework2DArrays/Homework2DArrays.java
@@ -14,13 +14,7 @@ import java.util.Scanner;
  *************************************************************************************** 
  *         REQUIRED HELP CITATION
  * 
- *         Help from anyone other than a CSSE220 Instructor or CSSE220 TA must
- *         be cited. If you didn't get any outside help, then you can simply
- *         state below: "I did not receive any outside help"
- ***************************************************************************************
- *         Help Citation (for this or any other files in this assignment)
- * 
- *         TODO: cite your help here
+ *         TODO: cite your help here or say "only used CSSE220 materials"
  *************************************************************************************** 
  */
 public class Homework2DArrays {
