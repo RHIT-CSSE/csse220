1464571b146880c48aaebb5c019a97777d1b05d8
Jason Yoder <jasonayoder@gmail.com>
2025-03-11 12:05:33 -0400

    fixing screenshot on slides

--- ClassMaterials/ArraysAnd2DArrays/EclipseProjects/PracticeArraysAnd2DArrays/src/Two2DArrayProblems.java
+++ ClassMaterials/ArraysAnd2DArrays/EclipseProjects/PracticeArraysAnd2DArrays/src/Two2DArrayProblems.java
@@ -40,6 +40,9 @@ public class Two2DArrayProblems {
 	 *  
 	 */
 	public static char[][] stringsToCharArrays(String[] input) {
+		
+	
+		
 
 		return null;
 	}
@@ -95,6 +98,33 @@ public class Two2DArrayProblems {
 	 */
 	public static int distanceToExit(char[][] input) {
 		
-		return 0;
+		int personRow=-1;
+		int personCol=-1;
+		//find the location of the person
+		for (int r=0; r<input.length; r++) {
+			for (int c=0; c<input[0].length; c++) {
+				if (input[r][c] == '@') {
+					personRow=r;
+					personCol=c;
+				}
+			}
+		}
+		
+		int minDistance=10000;
+		
+		//find the closest X
+		for (int r=0; r<input.length; r++) {
+			for (int c=0; c<input[0].length; c++) {
+				if (input[r][c] == 'X') {
+					int distance = Math.abs(personRow-r) + Math.abs(personCol-c);
+					if (distance < minDistance ) {
+						minDistance = distance;
+					}
+					
+				}
+			}
+		}
+		
+		return minDistance;
 	}
 }
