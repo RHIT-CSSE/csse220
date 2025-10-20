58ca64144d350aa1837b54377a5668774573ab14
Ian G. Ludden <iludden94@gmail.com>
2025-10-20 16:57:32 -0400

    Add Act20: Event-based Game Loop, Custom Except.s

---
+++ ClassMaterials/Act20/sol/advancedSolutionCustomException/FileBestScore.java
@@ -0,0 +1,118 @@
+package advancedSolutionCustomException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+/**
+ * 
+ * Imagine a game where players player 3 or 4 rounds, and their overall score
+ * is the sum of the rounds.
+ * 
+ * This program is designed to compute the best player from a file that looks
+ * like this:
+ * 
+ * PlayerA 3,4,5
+ * PlayerB 4,5,1,2
+ * PlayerC 4,4,4,4
+ * 
+ * The program works correctly except for 2 things:
+ * 
+ * 1.  Cheating players sometimes do more than 4 rounds.  The program should
+ * print a warning when a cheating player is uncovered - but continue the
+ * computation ignoring the cheating player.  
+ * 
+ * Do this by making the function computeTotal throw a IllegalArgumentException
+ * when given a cheating record.  computeBestScore should catch the exception,
+ * print a warning, and continue the computation.
+ *
+ * 2.  If the user enters an invalid file name, the program crashes.  Instead
+ * the program should say that the file does not exist and let the user enter
+ * a new name (as many times as necessary).  Catch a FileNotFoundException
+ * in main to do this.
+ *
+ * @author hewner.
+ *         Created Apr 21, 2017. Updated 2021
+ */
+public class FileBestScore {	
+
+	private int computeTotal(String allScoresString) throws TooManyScoresException {
+		String[] allScoresArray = allScoresString.split(",");
+		int total = 0;
+		
+		if (allScoresArray.length > 4) {
+			throw new TooManyScoresException(allScoresArray.length, 4);
+		}
+		
+		
+		for(String score : allScoresArray) {
+			total += Integer.parseInt(score.trim());
+		} // end for
+		return total;
+	} // computeTotal
+
+	//-------------------------------------------------------------------------------
+
+	private void computeBestScore(String filename) throws FileNotFoundException, IOException {
+		FileReader f1 = new FileReader(filename);
+		Scanner s1 = new Scanner(f1);
+		int bestScore = 0;
+		String bestPlayer = null;
+		
+		while(s1.hasNext()) {
+			String name = s1.next();
+			try {
+				String allScoresString = s1.nextLine();
+				int total = computeTotal(allScoresString);
+				if(total > bestScore) {
+					bestScore = total;
+					bestPlayer = name;
+				} // end if
+			} catch (TooManyScoresException e) {
+				System.err.println(name + " caught cheating with " +
+						e.getMessage() + " Skipping.");
+				
+			} catch (NumberFormatException e) {
+				System.err.println(name + " had a bad number");
+			} catch (NoSuchElementException e) {
+				System.err.println(name + " had no numbers!");
+			}
+			
+		} // end while
+		s1.close();
+		f1.close();
+		System.out.println("Best player " + bestPlayer + " with total score " + bestScore);
+	} // computeBestScore
+	
+	//-------------------------------------------------------------------------------
+
+	private void runApp()  {
+		Scanner s = new Scanner(System.in);
+		
+		boolean isDone = false;
+		while ( !isDone ) {
+			String filename = null;
+			System.out.println("What file should I load?  (e.g. scores.txt)");
+			filename = s.nextLine();
+			try {
+				computeBestScore(filename);
+				isDone = true;
+			} catch (FileNotFoundException e) {
+				System.err.println("File not found. Please try again.");
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		s.close();		
+		System.out.println("App terminated");
+	} // runIt
+	
+	//-------------------------------------------------------------------------------
+
+	public static void main(String[] args)  {
+		FileBestScore app = new FileBestScore();
+		app.runApp();
+	} // main
+}
---
+++ ClassMaterials/Act20/sol/gameEventLoopSol/Main.java
@@ -0,0 +1,78 @@
+package gameEventLoopSol;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+
+/**
+ * In this project, we build a simple framework for a game.
+ * This framework stores the state of the game. The framework 
+ * then repeatedly does these 3 things in one "tick" of the clock:
+ * 
+ * 1. Input events from the human change the game state. 
+ * 2. Things move/spawn/die according to the game state.
+ * 3. The component draws the current game state.
+ * 
+ * In this exercise, we develop each of these ideas.
+ * TODO: Replace the loop with button input to advance one tick.
+ * TODO: Replace the button with a timer.
+ * TODO: Make the button change the direction of motion.
+ * TODO: Add falling raindrops.
+ * TODO: Make the box "catch" drops that hit it.
+ *
+ * @author Matt Boutell, Mark Hays, Jason Yoder, Buffalo Hewner,
+ * and their colleagues.
+ *
+ */
+public class Main {
+
+	public static final int DELAY=50;
+	
+	public static void main(String[] args) {
+		new Main();
+	}
+
+	public Main() {
+		JFrame frame = new JFrame("Arcade Game");
+		frame.setSize(500, 500);
+		
+		GameComponent component = new GameComponent();
+		frame.add(component, BorderLayout.CENTER);
+	
+		JButton button = new JButton("Click me");
+		frame.add(button, BorderLayout.SOUTH);
+		GameAdvanceListener advanceListener = new GameAdvanceListener(component);
+		// 2. Add the advanceListener to the button.
+		//		button.addActionListener(advanceListener);
+		// 3. Add the advanceListener to a Timer
+		Timer timer = new Timer(DELAY, advanceListener);
+		timer.start();
+
+		// 4. Add this listener.
+		// 5. Tie in the raindrop class (other class)
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				component.toggleBoxDirection();
+			}
+		});
+		
+		
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//while (true) {
+		//	advanceListener.advanceOneTick();
+			// 1. How do we slow this down?
+			// NOTE: Could try to insert a big for loop here to slow 
+			// the processor enough to see the square.
+
+		//}
+		
+	}
+}
