84e6f7a03c822cf9cb0f5b49ce99ecdeab851b4e
Ian G. Ludden <iludden94@gmail.com>
2025-10-31 16:43:25 -0400

    Add Act25 Big-O and Sorting files

---
+++ ClassMaterials/Act25/src/counting/Loops.java
@@ -0,0 +1,181 @@
+package counting;
+
+import java.util.Random;
+
+/**
+ * This class provides several methods that take arrays as input. Your task is
+ * to analyze the methods.
+ * 
+ * @author Curt Clifton 
+ * Revised to remove generics by Jason Yoder
+ */
+public class Loops {
+	private static final int LARGEST_RANDOM_INTEGER = Integer.MAX_VALUE;
+	private static final int RANDOM_SEED = 220;
+
+	/**
+	 * Runs the sample code.
+	 * 
+	 * @param args
+	 *            ignored
+	 */
+	public static void main(String[] args) {
+		int n = 10;
+
+		int[] array = randomArray(n);
+
+		System.out.println(methodA(array));
+		System.out.println(methodB(array));
+		System.out.println(methodC(array));
+		System.out.println(methodD(array));
+		System.out.println(methodE(array));
+	}
+
+	private static int methodA(int[] array) {
+		/*
+		 * TODO Part 1: Here's an example of what your comments should look
+		 * like:
+		 *
+		 * Best case:
+		 * In the best case the array is accessed n + 1 times. Once before the
+		 * loop and n times in the loop. In the best case the comparison always
+		 * fails (i.e., the first element is the largest) so the array access
+		 * inside the "if" statement never happens.
+		 *
+		 * Worst case:
+		 * In the worst case the array is accessed 2*n times. Once before the
+		 * loop. n times in the if statement, which is called n times. In the
+		 * worst case (which is when the array is sorted), the comparison
+		 * succeeds every time except the first -- it must fail the first time
+		 * since array[0] == array[0] -- so the array access inside
+		 * the "if" statement happens n-1 times. The total is 1 + (n-1) + n =
+		 * 2*n.
+		 */
+		int n = array.length;
+		int count = 0;
+		if (n == 0)
+			return 0;
+		Integer result = array[0];
+		for (int i = 0; i < n; i++) {
+			count++;
+			if (result > array[i]) {
+				result = array[i];
+			}
+		}
+		return result;
+	}
+
+	private static int methodB(int[] array) {
+		/*
+		 * TODO Part 2: Add comments giving best and worst cases:
+		 * 
+		 * Best case:
+		 * 
+		 * 
+		 * Worst case:
+		 * 
+		 * 
+		 */
+		int n = array.length;
+		int count = 0;
+		if (n == 0)
+			return 0;
+		int result = array[0];
+		for (int i = 0; i < n; i++) {
+			count++;
+			if (result >= array[i] ) {
+				result = array[i];
+			}
+		}
+		return result;
+	}
+
+	private static int methodC(int[] array) {
+		/*
+		 * TODO Part 3: Add comments giving best and worst cases:
+		 * 
+		 * Best case:
+		 * 
+		 * 
+		 * Worst case:
+		 * 
+		 * 
+		 */
+		int n = array.length;
+		int count = 0;
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				count++;
+				if (array[j] > array[i] ) {
+					sum++;
+				}
+			}
+		}
+		return sum;
+	}
+
+	private static  int methodD(int[] array) {
+		/*
+		 * TODO Part 4: Add comments giving best and worst cases:
+		 * 
+	     * Best case:
+		 * 
+		 * 
+		 * Worst case:
+		 * 
+		 * 
+		 */
+		int n = array.length;
+		int count = 0;
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				count++;
+				if (array[j] > array[i]) {
+					sum++;
+				}
+			}
+		}
+		return sum;
+	}
+
+	private static  int methodE(int[] array) {
+		/*
+		 * TODO Part 5: Add comments giving best and worst cases:
+		 * 
+		 * Best case:
+		 * 
+		 * 
+		 * Worst case:
+		 * 
+		 * 
+		 */
+		int n = array.length;
+		int count = 0;
+		int sum = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) {
+				count++;
+				if (array[j] > array[i]) {
+					sum++;
+				}
+			}
+		}
+		return sum;
+	}
+
+	/**
+	 * @param n
+	 * @return an array of size n filled with randomly generated numbers
+	 */
+	private static int[] randomArray(int n) {
+		Random rand = new Random(RANDOM_SEED);
+		int[] result = new int[n];
+		for (int i = 0; i < n; i++) {
+			result[i] = rand.nextInt(LARGEST_RANDOM_INTEGER);
+		}
+		return result;
+	}
+
+}
---
+++ ClassMaterials/Act25/src/simple/LinkedList.java
@@ -0,0 +1,232 @@
+package simple;
+
+public class LinkedList {
+
+	private class Node {
+		public int value;
+		public Node next;
+
+		public Node(int value, Node next) {
+			this.value = value;
+			this.next = next;
+		}
+	}
+
+	private Node head;
+
+	public LinkedList() {
+		head = null;
+	}
+
+	/**
+	 * 
+	 * Adds a value at the beginning of the list
+	 * 
+	 * @param value
+	 */
+	public void addAtBeginning(int value) {
+		Node newNode = new Node(value, this.head);
+		this.head = newNode;
+	}
+
+	/**
+	 * Converts the list to a string like this: 1->2->3->null
+	 */
+	public String toString() {
+		Node current = this.head;
+		String result = "";
+		while (current != null) {
+			result += current.value;
+			result += "->";
+			current = current.next;
+		}
+		result += "null";
+		return result;
+	}
+
+	/**
+	 * 
+	 * Example of how the linked list is used
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		LinkedList list = new LinkedList();
+//		list.addAtBeginning(1);
+//		list.addAtBeginning(2);
+//		list.addAtBeginning(3);
+//		System.out.println(list.toString());
+
+		// See TODO #1 below complete addAtEnd()
+		// Add ALL values from 1 to N to a list
+		// Compare how long it takes using:
+		
+		// Try comparing how long it takes using addAtBeginning() and addAtEnd()
+		// Should there a difference? Why or why not?
+		// Is there a difference? Big/small?
+		
+		//Try these values for LIST_SIZE" {10, 100, 1000, 10000, 100000, 1000000}
+		int LIST_SIZE = 10;
+		
+		// This class will allow us to easily see how long it takes to run some code
+		StopWatch timer = new StopWatch();
+		timer.start();
+		// do stuff
+		timer.stop();
+		double secondsTaken = timer.getElapsedTime()/1000.0;
+		System.out.println("Took "+secondsTaken + " to do stuff");
+		timer.reset(); //reset the timer to use again
+		
+		// TODO #2 - add LIST_SIZE elements to a list using addAtEnd
+
+		// TODO #3 - add LIST_SIZE elements to a list using addAtBeginning
+
+		// TODO #4 - Time different methods of adding to the list
+
+
+	}
+
+	/**
+	 * Returns the number of elements in the linked list
+	 * 
+	 * The code for this will look a little bit like toString
+	 * 
+	 * @return
+	 */
+	public int size() {
+		return 0;
+	}
+
+	/**
+	 * 
+	 * Add value at the end of the linked list
+	 * 
+	 * To solve this problem you'll need a loop to find the last element of the
+	 * list.
+	 * 
+	 * You'll also need a special case for the empty list.
+	 * 
+	 * @param value
+	 */
+	public void addAtEnd(int value) {
+		// TODO #1 - complete this method
+	}
+
+	/**
+	 * Removes the first element from the list
+	 * 
+	 * If the list is empty, throw an exception like this: throw new
+	 * RuntimeException("Attempt to remove from an empty list");
+	 */
+	public void removeBeginning() {
+
+	}
+
+	/**
+	 * Removes the last element from the list
+	 * 
+	 * You'll need a special case for the 1 element list
+	 * 
+	 * If the list is empty, throw an exception like this: throw new
+	 * RuntimeException("Attempt to remove from an empty list");
+	 */
+	public void removeEnd() {
+
+	}
+
+	/**
+	 * 
+	 * Removes all the nodes with a particular value from the list
+	 * 
+	 * For example, given the list
+	 * 
+	 * 1->2->3->2->null
+	 * 
+	 * Removing all the 2s would result in: 1->3->null
+	 * 
+	 * @param nodesWithValue
+	 */
+	public void removalAll(int nodesWithValue) {
+
+	}
+
+	/**
+	 * 
+	 * Inserts a particular value after another value in the list
+	 * 
+	 * For example, in the list
+	 * 
+	 * 1->2->3->null
+	 * 
+	 * Adding the value 77 after the 2 would give
+	 * 
+	 * 1->2->77->3->null
+	 * 
+	 * You can assume the value will definitely be in the list
+	 * 
+	 * @param valueToAdd
+	 * @param valueToInsertAfter
+	 */
+	public void addAfterValue(int valueToAdd, int valueToInsertAfter) {
+
+	}
+
+	/**
+	 * 
+	 * Inserts a particular value before another value in the list
+	 * 
+	 * For example, in the list
+	 * 
+	 * 1->2->3->null
+	 * 
+	 * Adding the value 77 before the 2 would give
+	 * 
+	 * 1->77->2->3->null
+	 * 
+	 * You can assume the value will definitely be in the list
+	 * 
+	 * To solve this, you'll need to keep track of both a current and previous node
+	 * as you go through the list.
+	 * 
+	 * You'll also need a special case for the head of the list.
+	 * 
+	 * @param valueToAdd
+	 * @param valueToInsertBefore
+	 */
+	public void addBeforeValue(int valueToAdd, int valueToInsertBefore) {
+
+	}
+
+	/**
+	 * Removes the element after a particular value in the list
+	 * 
+	 * For example, given this this list: 1->2->3->null
+	 * 
+	 * Removing after the element 1 would result in: 1->3->null
+	 * 
+	 * 
+	 * You can assume that both the element and the element after exist in the list.
+	 * 
+	 * @param valueToRemoveAfter
+	 */
+	public void removeAfter(int valueToRemoveAfter) {
+
+	}
+
+	/**
+	 * 
+	 * This constructor takes and array and makes a linked list containing all the
+	 * elements in the array.
+	 * 
+	 * You could do this by repeatedly calling add at end but be classy and
+	 * construct the list by hand.
+	 * 
+	 * You can assume the array has at least one element.
+	 * 
+	 * @param data
+	 */
+	public LinkedList(int[] data) {
+
+	}
+
+}
