d3401e77a4935d91b4d00074d70b07bf91caeebf
Ian G. Ludden <iludden94@gmail.com>
2026-08-26 16:23:43 -0400

    Convert JavaIntro to IntelliJ, add genAI activity

---
+++ ClassMaterials/JavaIntro/SourceCode/PracticeJavaIntroPOGIL/src/ConvertToJava.java
@@ -0,0 +1,38 @@
+/**
+ * Template for converting from another language to Java. 
+ * 
+ * @author Ian Ludden
+ */
+public class ConvertToJava {
+
+    public static void main(String[] args) {
+        // Create myFunction as a static function in this same class, adding/modifying parameters/arguments as needed. 
+        // Example: 
+        // myFunction(25.0); // Modify this to test your function. 
+
+        // or, if you want to return and print some value:
+        // [datatype] result = myFunction([arguments]);
+        // System.out.println(result);
+    }
+
+    // TODO: Add your converted function here. 
+
+    // === EXAMPLE ===
+    // Python version: 
+    // def celsiusToFahrenheit(celsius: float) -> None:
+    //   fahr = celsius * 9.0 / 5 + 32
+    //   print("{0:.1f} C = {1:.1f} F".format(celsius, fahr))
+    // 
+    // Java version: 
+    // /**
+    //  * Convert Celsius to Fahrenheit and print result
+    //  * @param celsius - temperature in Celsius
+    //  */
+    // public static void myFunction(double celsius) {
+    //     double fahr; // temp in fahrenheit
+    //     String str;
+    //     fahr = celsius * 9.0 / 5 + 32;
+    //     str = String.format("%.1f C = %.1f F\n", celsius, fahr);
+    //     System.out.println(str);
+    // } 
+}
